913754226f27a761bec5d8d386adefd99e21646c
raushan778 <allmusiccenter404@gmail.com>
2025-05-18 19:43:14 +0530

    starting project on resume builder

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl text-rose-300'>Hello Khiladi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
